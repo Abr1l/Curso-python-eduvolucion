ca74f5ea3dd85548ba933b03c7d9781be488a188
Nazaret Lopez <milinazaret@gmail.com>
2017-02-11 14:46:26 -0400

    culminación de directorio

--- Directorio Telefónico/directorio.py
+++ Directorio Telefónico/directorio.py
@@ -11,8 +11,11 @@
 #entrada y salida de ficheros. Además de los conceptos aprendidos en los capítulos anteriores
 
 
+import os
+#El módulo os nos permite acceder a funcionalidades dependientes del Sistema Operativo. Sobre todo, aquellas que nos refieren información sobre el entorno del mismo y nos permiten manipular la estructura de directorios (para leer y escribir archivos
 
 import csv
+
 directorio = {}
 
 def mostrar_menu():
@@ -20,7 +23,7 @@ def mostrar_menu():
 	print("2- Agregar número telefónico")
 	print("3- Eliminar número telefónico")
 	print("4- Buscar número telefónico")
-	print("5- Importar a un archivo csv el directorio telefónico")
+	print("5- Guardar directorio telefónico")
 	print("6- Salir ")
 	
 
@@ -29,5 +32,36 @@ def imprimirDirectorio():
 	for x in directorio.keys():
 		print("Nombre: " + x + "\tNúmero: " + directorio[x])
 		print()
+	
+def agregarContacto():
+	print("Agregar contacto")
+	nombre = raw_input("Nombre del contacto: ")
+	telefono = raw_input("Número de teléfono: ")
+	directorio[nombre] = telefono
+
+def eliminarContacto():
+	print("Eliminar contacto")
+	nombre = raw_input ("indica el nombre eliminar: ")
+	if nombre in directorio:
+		del directorio[nombre]
+	else:
+		print("¡El nombre no fue encontrado!")
+
+def buscarContacto():
+	print("Buscar contacto")
+	nombre = raw_input ("indica el nombre eliminar: ")
+	if nombre in directorio:
+		print("El número es: " + directorio[nombre])
+	else:
+		print("¡" + nombre + " no fue encontrado!")
+
+def guardarArchivo():
+	print("Guardar Archivo")
+	if not os.path.exists("directorio.csv"): #os.path.exists saber si existe el archivo
+		archivocsv = csv.writer(open("directorio.csv" "wb")) #abre con permiso de escritura, si no existe, lo crea.
+	
+	for (clave, valor) in directorio.items(): # items, Obtener las claves y valores de un diccionario
+		archivocsv.writerow([clave,valor]) 
+	
 	
 	
